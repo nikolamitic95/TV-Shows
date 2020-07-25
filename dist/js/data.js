98a83159ab88804c70cfdb426633068953d8d588
Nikola Mitic <nikola.mitic1995@gmail.com>
2020-07-25 14:21:33 +0200

    run

--- dist/js/data.js
+++ dist/js/data.js
@@ -1,49 +1,49 @@
 import { fetch } from './fetch.js';
 
 export const fetchShows = (onSuccess) => {
-    fetch("https://api.tvmaze.com/shows", (data) => {
+    fetch("http://api.tvmaze.com/shows", (data) => {
         onSuccess(data);
     })
 }
 
 export const fetchShow = (id, onSuccess) => {
-    fetch(`https://api.tvmaze.com/shows/${id}`, (data) => {
+    fetch(`http://api.tvmaze.com/shows/${id}`, (data) => {
         onSuccess(data);
     })
 }
 
 export const fetchSearchShow = (str, onSuccess) => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${str}`, (data) => {
+    fetch(`http://api.tvmaze.com/search/shows?q=${str}`, (data) => {
         onSuccess(data)
     })
 }
 
 export const fetchShowSeasons = (id, onSuccess) => {
-    fetch(`https://api.tvmaze.com/shows/${id}/seasons`, (data) => {
+    fetch(`http://api.tvmaze.com/shows/${id}/seasons`, (data) => {
         onSuccess(data);
     })
 }
 
 export const fetchCast = (id, onSuccess) => {
-    fetch(`https://api.tvmaze.com/shows/${id}/cast`, (data) => {
+    fetch(`http://api.tvmaze.com/shows/${id}/cast`, (data) => {
         onSuccess(data)
     })
 }
 
 export const fetchCrew = (id, onSuccess) => {
-    fetch(`https://api.tvmaze.com/shows/${id}/crew`, (data) => {
+    fetch(`http://api.tvmaze.com/shows/${id}/crew`, (data) => {
         onSuccess(data)
     })
 }
 
 export const fetchAka = (id, onSuccess) => {
-    fetch(`https://api.tvmaze.com/shows/${id}/akas`, (data) => {
+    fetch(`http://api.tvmaze.com/shows/${id}/akas`, (data) => {
         onSuccess(data);
     })
 }
 
 export const fetchEpisodes = (id, onSuccess) => {
-    fetch(`https://api.tvmaze.com/seasons/${id}/episodes`, (data) => {
+    fetch(`http://api.tvmaze.com/seasons/${id}/episodes`, (data) => {
         onSuccess(data);
     })
 }
